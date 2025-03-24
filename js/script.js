83a375ce75325c8f32a8ad66fe60d72622ecd2f7
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-24 15:36:07 +0100

    [UPD] Second part

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@ let isPalindroma = checkPalindromo(parola)
 
 //Stampare messaggio all'utente
 alert((isPalindroma) ? (`La stringa e' palindorma`) : (`La stringa non e' palindorma`))
+
+//Pari e Dispari
+
+//Prompt di scelta per l'utente tra 'pari' o 'dispari'
+
+//Inserimento valore da 1 a 5
+//Funzione per selezionare un numero random per il PC da 1 a 5
+//Somma dei due numeri
+//Funzione per stabilire se la somma e' pari o dispari
+//Funzione per stabilire il vincitore
